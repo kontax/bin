3eebcfba99af125cb32ad409fbcb7c82a97a265a
James Coulson <coulsonj@gmail.com>
2014-02-02 13:52:09 +0000

    updated vimrc with tabstop=4

--- workconnect.sh
+++ workconnect.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # connect to work desktop through laptop
 # laptop must be on and connected via ssh
-
-rdesktop -u jcoulson -p $PASSWORD -g 1280x1024 -f localhost
+set -x
+rdesktop -d PPOWER -u jcoulson -p $PASSWORD -g 1920x1080 -f localhost
