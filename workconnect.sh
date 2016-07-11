e8cfd9119e6b9579f0d65ef350ce6accd6ad3887
James Coulson <coulsonj@gmail.com>
2016-07-11 15:27:16 +0100

    Altering VBox run script and workconnect

--- workconnect.sh
+++ workconnect.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 # connect to work desktop through laptop
 # laptop must be on and connected via ssh
-set -x
+
+#set -x
+read -s -p "Password: " PASSWORD
+echo 
+#exec > /dev/null 2>&1
 rdesktop -d PPOWER -u jcoulson -p $PASSWORD -g 1920x1080 -f localhost
