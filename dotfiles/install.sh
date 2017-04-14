4854cc44e06d81ca4dfceeda01949b89a3cb7306
James Coulson <coulsonj@gmail.com>
2017-04-14 09:57:38 +0100

    Updating dotfiles so vim is more modular, as well as adding a small install script

---
+++ dotfiles/install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# 
+# install.sh
+# Small script used to install the dotfiles once the directory has been cloned
+
+
+#
+# Vim
+#
+
+ln -s ~/bin/dotfiles/vim/dot_vim ~/.vim             # Vim config directory
+ln -s ~/bin/dotfiles/vim/vimrc ~/.vimrc             # vimrc
+
+
+#
+# Git
+#
+
+## Add hooks here for ctags
+
+#
+# GDB
+# 
---
+++ dotfiles/vim_new/modules/netrw.vim
@@ -0,0 +1,6 @@
+" --------------------------------
+" Settings for NetRW file explorer
+" --------------------------------
+
+let g:netrw_liststyle = 3                   " Use the tree view style
+let g:netrw_banner = 0                      " Remove the banner
